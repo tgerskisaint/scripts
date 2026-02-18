f373c450940bcd92131cc73adf9d514155305308
tgerskisaint <tboone729@gmail.com>
2026-02-18 14:27:52 -0600

    removed bad script

--- daily-reminders.py
+++
@@ -1,9 +0,0 @@
-import time
-
-# Water intak eremionder
-def water_reminder():
-    while True:
-        print("Sip water or die hoe")
-        time.sleep(3600)
-
-water_reminder() 
--- daily-reminders01.py
+++
@@ -1,9 +0,0 @@
-from python_cron import Cron
-
-def water_reminder():
-    while True:
-        print("Sip water or die hoe")
-
-cron = Cron()
-cron.schedule('* * * * *', water_reminder)
-cron.start()
